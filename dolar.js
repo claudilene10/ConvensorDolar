e425cc5891807a71210c78fdbe6794a7f94a2b81
CLAUDILENE PONCIANO DOS SANTOS <claudilene.psantos@senacsp.edu.br>
2025-05-20 11:05:57 -0300

    Conversor

---
+++ dolar.js
@@ -0,0 +1,13 @@
+function converter() {
+    const valorDolar = parseFloat(document.getElementById("valorDolar").value);
+    const taxaCambio = 5.65;
+  
+    if (isNaN(valorDolar)) {
+      document.getElementById("resultado").textContent = "Por favor, digita um valor válido.";
+      return;
+    }
+  
+    const valorReal = valorDolar * taxaCambio;
+    document.getElementById("resultado").textContent = 
+      `US$ ${valorDolar.toFixed(2)} = R$ ${valorReal.toFixed(2)}`;
+  }
